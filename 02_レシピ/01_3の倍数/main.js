daed94ebdef37d8092f77a6650b95d3f81a00b15
Aoi0963 <saa_okumura@cc.nara-wu.ac.jp>
2021-07-03 21:55:11 +0900

    [□と○]を学習しました

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,10 @@
+const genkiFunction = function(number) {
+  genkiFunction(24)
+  for (let n = 1; n <= number; n++) {
+    if (number % 3 === 0) {
+      console.log(number + "!!!!!!")
+    } else {
+      console.log(number)
+    }
+  }
+}
